42d33fe84d96e58aeebc301d89a6bdcdd4b8f37e
Istomin Alexey <tmozzze@gmail.com>
2025-09-26 00:12:12 +0300

    UPDATE: L1_21

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	l121 "github.com/tmozzze/WB_techoschool/L1/L1_21"
+)
+
 func main() {
-	dog := l121.Dog{}
+	var c1, c2 l121.Creature
+
+	c1 = &l121.Human{}
+	c1.Say()
+
+	dog := &l121.Dog{}
+	dog.Bark()
+
+	c2 = &l121.DogAdapter{DogExample: dog}
+	c2.Say()
+
 }
